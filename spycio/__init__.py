bc8b1bfbbac0bd6c6a656cad3fe1c03efeaf73fa
Bruno Peixoto <brunolnetto@gmail.com>
2023-04-01 12:07:34 -0300

    feat/ structure for examples, main folder and tests

---
+++ spycio/__init__.py
@@ -0,0 +1,8 @@
+""" Package Eule  """
+
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+
+__author__ = """Bruno Peixoto"""
+__email__ = 'brunolnetto@gmail.com'
---
+++ tests/test_spycio.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+import pytest
+
+
+def test_spycio():
+    """
+    Generates a tuple with key-value
+    """
+    assert "ackbar" == "ackbar"
